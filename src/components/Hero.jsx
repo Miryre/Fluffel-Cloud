52c8436d9ae8aea4a7cdd433d9c53788587c8873
Elizabeth Reynolds <e.d.reynolds@icloud.com>
2025-10-17 11:27:22 -0500

    completed hero structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import WeatherCard from './WeatherCard';
 
 const Hero = () => {
   return (
-    <div className='relative bg-gray-900 text-white min-h-screen flex flex-col justify-center items-center py-8'>
+    <div className='relative bg-gray-900 text-white h-[75vh] flex flex-col justify-start items-center pt-20 pb-8'>
       {/* Background Video*/}
       <video
         className='absolute top-0 left-0 w-full h-full object-cover z-0'
@@ -14,16 +14,20 @@ const Hero = () => {
       ></video>
       
       {/*Hero Content*/}
-      <div className='relative z-10 w-full px-4'>
-        <h1 className='text-4xl md:text-6xl lg:text-7xl font-bold mb-2 text-center'>
-          Welcome to Fluffel Cloud
-        </h1>
-        <p className='text-base md:text-xl lg:text-2xl text-center mb-6'>
-          Your easy weather forecast.
-        </p>
+      <div className='relative z-10 w-full px-4 flex flex-col h-full'>
+        <div className='mb-4'>
+          <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold mb-3 text-center'>
+            Welcome to Fluffel Cloud
+          </h1>
+          <p className='text-sm md:text-base lg:text-xl text-center'>
+            Your easy weather forecast.
+          </p>
+        </div>
         
         {/* Weather Card goes here */}
-        <WeatherCard />
+        <div className='flex-1 flex items-center'>
+          <WeatherCard />
+        </div>
       </div>
     </div>
   );
