00e069475fc8858b11c093e03a2b59c1f65644fb
Elizabeth Reynolds <e.d.reynolds@icloud.com>
2025-10-17 11:46:42 -0500

    added useState to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import React, { useState } from 'react';
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 
 function App() {
+// state to hold the city name
+    const [city, setCity] = useState('Dallas');
+
+// state to hold weather data
+    const [weatherData, setWeatherData] = useState(null);  
+
     return(
         <div>
             <Navbar />
